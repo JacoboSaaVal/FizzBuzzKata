19621111e8045d50ece1d06d6dd04c9ff55f40f9
JacoboSaaVal <jacobosaaval@gmail.com>
2025-04-23 18:46:08 +0100

    Tests extended

--- FizzBuzz.Tests/FizzBuzzerTests.cs
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -7,13 +7,41 @@ public class FizzBuzzerTests
 {
     [Theory]
     [InlineData(1, "1")]
-    [InlineData(3, "Fizz")]
-    [InlineData(5, "Buzz")]
-    [InlineData(15, "FizzBuzz")]
-    [InlineData(16, "16")]
-    public void GetValue_ReturnsExpectedResult(int input, string expected)
+    [InlineData(2, "2")]
+    [InlineData(4, "4")]
+    public void GetValue_ReturnsNumber_WhenNotDivisibleBy3Or5(int input, string expected)
     {
         var result = FizzBuzzer.GetValue(input);
         Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(6)]
+    [InlineData(9)]
+    public void GetValue_ReturnsFizz_WhenDivisibleBy3(int input)
+    {
+        var result = FizzBuzzer.GetValue(input);
+        Assert.Equal("Fizz", result);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(20)]
+    public void GetValue_ReturnsBuzz_WhenDivisibleBy5(int input)
+    {
+        var result = FizzBuzzer.GetValue(input);
+        Assert.Equal("Buzz", result);
+    }
+
+    [Theory]
+    [InlineData(15)]
+    [InlineData(30)]
+    [InlineData(45)]
+    public void GetValue_ReturnsFizzBuzz_WhenDivisibleBy3And5(int input)
+    {
+        var result = FizzBuzzer.GetValue(input);
+        Assert.Equal("FizzBuzz", result);
     }
 }
